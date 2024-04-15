6684a3b604a61bea2caf322e5d4353a88b368358
pixelabra <a3y3g1@gmail.com>
2024-04-16 02:26:29 +0400

    finalised makefile

--- fdf/includes/fdf.h
+++ fdf/includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: a3y3g1 <a3y3g1@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 23:51:56 by a3y3g1            #+#    #+#             */
-/*   Updated: 2024/04/15 23:54:56 by a3y3g1           ###   ########.fr       */
+/*   Updated: 2024/04/16 02:01:28 by a3y3g1           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include "../minilibx_PxAb/mlx.h"
+# include "../libft/libft.h"
 
 #endif
--- fdf/srcs/fdf.c
+++ fdf/srcs/fdf.c
@@ -6,7 +6,7 @@
 /*   By: a3y3g1 <a3y3g1@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 13:40:35 by a3y3g1            #+#    #+#             */
-/*   Updated: 2024/04/16 01:52:36 by a3y3g1           ###   ########.fr       */
+/*   Updated: 2024/04/16 02:26:07 by a3y3g1           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int	main(void)
 	mlx_win = mlx_new_window(mlx, 400, 400, "MLX_WINDOW");
 	if (!mlx_win)
 		return (2);
+	mlx_clear_window(mlx, mlx_win);
+	// mlx_hook(mlx_win, 17, 0, m, mlx);	
 	mlx_loop(mlx);
 	return (0);
 }
