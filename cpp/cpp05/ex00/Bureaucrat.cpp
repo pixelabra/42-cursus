c2a613b9a0a02edae57f1971d9edaec21c652a5b
Alexandru Godeanu <agodeanu@lab1r4s14.42abudhabi.ae>
2025-04-06 20:18:51 +0400

    threw a few things at the bureaucrat

--- cpp/cpp05/ex00/Bureaucrat.cpp
+++ cpp/cpp05/ex00/Bureaucrat.cpp
@@ -6,15 +6,24 @@
 /*   By: agodeanu <agodeanu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 20:35:30 by agodeanu          #+#    #+#             */
-/*   Updated: 2025/04/05 21:25:04 by agodeanu         ###   ########.fr       */
+/*   Updated: 2025/04/06 19:30:40 by agodeanu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat()
+Bureaucrat::Bureaucrat(): name("basic brrcrat"), grade(150)
 {
-	std::cout << "Default bureaucrat activated." << std::endl;
+	std::cout << "Basic constructor brrcrat activated." << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string _name, int _grade)
+{
+	std::cout << "Level brrcrat activated." << std::endl;
+	if (_grade > 150)
+		throw GradeTooLowException();
+	if (_grade < 1)
+		throw GradeTooHighException();
 }
 
 Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& other)
@@ -25,15 +34,30 @@ Bureaucrat&	Bureaucrat::operator=(const Bureaucrat& other)
 	return (*this);
 }
 
-std::stream&	Bureaucrat::operator<<(std::ostream& stream, const Bureaucrat& other)
+std::ostream&	operator<<(std::ostream& stream, const Bureaucrat& bcrat)
 {
-
+	std::cout << bcrat.getName() << ", bureaucrat grade " << bcrat.getGrade() << std::endl;
+	return (stream);
 }
 
 Bureaucrat::~Bureaucrat()
 {
-	std::cout << "Bureacratopocalypse" << name << "\n";
+	std::cout << "Brrcratopocalypse: " << name << " at grade " << grade << "\n";
 }
 
-std::string	Bureaucrat::getName() const { return (name); }
-int			Bureaucrat::getGrade() const { return (grade); }
+const std::string&		Bureaucrat::getName() const { return (name); }
+const int&				Bureaucrat::getGrade() const { return (grade); }
+
+void	Bureaucrat::promote()
+{
+	if (grade <= 1)
+		throw GradeTooHighException();
+	grade--;
+}
+
+void	Bureaucrat::demote()
+{
+	if (grade >= 150)
+		throw GradeTooLowException();
+	grade++;
+}
--- cpp/cpp05/ex00/Bureaucrat.hpp
+++ cpp/cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: agodeanu <agodeanu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 20:34:50 by agodeanu          #+#    #+#             */
-/*   Updated: 2025/04/05 21:24:45 by agodeanu         ###   ########.fr       */
+/*   Updated: 2025/04/06 19:30:45 by agodeanu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define BUREAUCRAT_HPP
 
 # include <iostream>
+# include <exception>
 
 class Bureaucrat
 {
@@ -22,14 +23,33 @@ class Bureaucrat
 		int					grade;
 	public:
 		Bureaucrat();
-		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
 		Bureaucrat(const Bureaucrat& other);
 		Bureaucrat&		operator=(const Bureaucrat& other);
-		std::stream&	operator<<(std::ostream& stream, const Bureaucrat& other);
 		virtual ~Bureaucrat();
 
-		std::string	getName() const;
-		int			getGrade() const;
+		const std::string&		getName() const;
+		const int&				getGrade() const;
+
+		void					promote();
+		void					demote();
+
+	class GradeTooHighException: public std::exception
+	{
+		private:
+			std::string	message;
+		public:
+			GradeTooHighException(): message("Grade is too high") {}
+			const char* what() const noexcept { return (message.c_str()); }
+	};
+	class GradeTooLowException: public std::exception
+	{
+		private:
+			std::string	message;
+		public:
+			GradeTooLowException(): message("Grade is too low") {}
+			const char* what() const { return (message.c_str()); }
+	};
 };
 
 #endif
